c4a80f6f8d65c90add82fefe84077ed9e91bbefa
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-07-13 20:49:22 +0100

    kubecfg: add flag -i (interactive)

--- autokubeconfig.sh
+++ autokubeconfig.sh
@@ -18,6 +18,7 @@ function kubecfg()
     return
   fi
 
+  # Unset KBUECONFIG env
   if [ "$1" == -u ]; then
     if [ -v KUBECONFIG ]; then
       echo Unsetting KUBECONFIG=$KUBECONFIG
@@ -26,13 +27,34 @@ function kubecfg()
     return
   fi
 
-  export KUBECONFIG="$(realpath $1)"
-
-  if ! [ -e "$1" ]; then
-    echo Creating kubeconfig: $1
-    touch $1
+  # Interactive select kubeconfig file
+  if [ "$1" == -i ]; then
+    local config
+    local found=()
+    for config in ./.kubeconfig* ./kubeconfig* ./.kube/config*; do
+      [ -e "$config" ] && found+=("$config")
+    done
+    if [ ${#found[*]} -eq 0 ]; then
+      echo No suitable kubeconfig file found
+      return
+    fi
+    if type fzf 2>/dev/null; then
+      KUBECONFIG=$(printf "%s\n" "${found[@]}" | fzf --select-1 --reverse)
+      export KUBECONFIG="$(realpath "$KUBECONFIG")"
+    else
+      select config in "${found[@]}"; do
+        export KUBECONFIG="$(realpath $config)"
+      done
+    fi
   else
-    chmod 700 $KUBECONFIG
+    export KUBECONFIG="$(realpath "$1")"
+
+    if ! [ -e "$1" ]; then
+      echo Creating kubeconfig: $1
+      touch "$1"
+    else
+      chmod 700 "$KUBECONFIG"
+    fi
   fi
 
   echo -e "\033[32mUsing kubeconfig: ${KUBECONFIG}\033[0m"
